22e2ff7f090633901920d38cd7d1d4f9aae9f156
chuchu <chuchu@rands-air.lan>
2025-01-30 10:38:36 -0500

    Homework

---
+++ src/Demo/HW4.java
@@ -0,0 +1,20 @@
+package Demo;
+
+public class HW4 {
+
+	public static void main(String args[]) {
+
+		int c = 77;
+		int n = 98;
+
+		if (n == c) {
+			System.out.println("Equal");
+
+		} else {
+			System.out.println("incorrect");
+
+		}
+
+	}
+
+}
